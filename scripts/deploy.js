c7913790e0c448111679c1163e4accbe0dd30d4a
alexlu0917 <alexlu0917.dev@gmail.com>
2022-03-11 02:34:00 +0000

    add the deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const {ethers, upgrades} = require("hardhat");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Box = await ethers.getContractFactory("Box");
+  console.log("Deploing Box...")
+  const box = await upgrades.deployProxy(Box, [42], {initializer: 'store'});
+  await box.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Box deployed to:", box.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
