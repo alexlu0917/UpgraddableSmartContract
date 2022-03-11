71b95046c3cd7c14ca5c73a166f2fca42be20abf
alexlu0917 <alexlu0917.dev@gmail.com>
2022-03-11 02:25:52 +0000

    add the example smart contract

---
+++ contracts/Box.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Box {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    function store(uint256 value) public {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return _value;
+    }
+}
